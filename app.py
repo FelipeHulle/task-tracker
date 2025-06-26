33b9047b584cea120de743b1522241f8fc205431
Felipe Hulle <felipehulle@hotmail.com>
2025-06-25 23:56:58 -0300

    creation of the add method and code modularization

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+#%%
 import json
+import os
+from datetime import datetime
 
 class PlayerInterface:
 
@@ -9,26 +12,98 @@ class PlayerInterface:
 class Engine:
     
     def __init__(self):
-        self.idx = 0
+        pass
+
+    def _agora(self):
+        return datetime.now()
+
+    def _read_database(self,path = 'data.json') -> json:
+        """
+        Read data in json database
+        
+        """
+        if not os.path.exists(path):
+            with open(path, 'x') as file:
+                return {}
+            
+        if os.path.getsize(path) == 0:
+            return {}
+        
+        with open(path,'r') as file:
+            saved_json = json.load(file)
+            return saved_json
+
+        
+    def _save_data_in_database(self,dados: json,path = 'data.json') -> str:
+        """
+        Save data in json database
+        """
+
+        old_data = self._read_database(path)
+        
+        if os.path.getsize(path) == 0:
+            old_data = []
+
+        if not isinstance(old_data,list):
+            old_data = [old_data]
+
+        old_data.append(dados)        
+
+        with open(path,'w') as file:
+            json.dump(old_data,file,indent=4)
+            return 'Sucess'
+
 
     def add(self,description,status='todo'):
+        """
+        Create new item
+        """
+         
+        try:
+            id = int(self._read_database()[-1].get('id')) + 1
+        except KeyError:
+            id = 0
+
 
         data = {}
-
-        data['id'] = self.idx
+        data['id'] = id
         data['description'] = description
         data['status'] = status
+        data['createdAt'] = str(self._agora())
+        data['updatedAt'] = str(self._agora()) 
 
-        with open('data.json','a') as file:
-            json.dump(data,file,indent=2)
-        
-        self.idx += 1
+        saving = self._save_data_in_database(data)
 
-        return 0
+        mensagem = f'Item {id} foi adicionado com sucesso.'
+
+        return mensagem
     
-if __name__ == '__main__':
-    engine = Engine()
-    engine.add('estudar python')
+    def update(self,id,description):
+        """
+        Update item
+        """
+
+        old_item = next((item for item in self._read_database() if item.get('id') == id),None)
+        
+        old_item['description'] = description
+        old_item['updatedAt'] = str(self._agora())
+
+        return print(old_item)
+
+
+
+#%%
+
+engine = Engine()
+# engine._read_database()
+# engine.add('Macetar')
+# engine.update(12,'comer')
+
+
+
+
+#%%
+
 
 # id: A unique identifier for the task
 # description: A short description of the task
